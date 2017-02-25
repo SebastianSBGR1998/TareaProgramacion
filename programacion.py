597328a45ffc781000aec5a7e09f0b65fa2d4d38
SebastianSBGR1998 <sebastianamezquitaleon@gmail.com>
2017-02-25 18:38:48 -0500

    Se realiza el primer push de un codigo de fuente
    Por medio de este pequeño push se podria decir que
    ya estoy manejando mejor tanto git como python
    
    Adicional a esto este archivo contiene una linea
    de codigo la cual puede encontrar cuantas son las
    letras que hay en una palabra.

---
+++ programacion.py
@@ -0,0 +1,16 @@
+# Variables
+palabra = "Programacion"
+contador = 0
+b = "o"
+
+# Proceso
+
+print (palabra)
+
+for i in palabra:
+	if i == b:
+		contador = (contador+1)
+		
+
+print  (contador, " " + "son las letras O que hay en la palabra" + " " + palabra)
+
